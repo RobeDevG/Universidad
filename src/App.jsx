31e014c742a3e803a9c42a3d092f06d9aab453df
RobeDevG <robertodev97@gmail.com>
2024-12-10 11:54:48 -0500

    somechanges

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,13 @@ import Home from './component/Home'
 
 
 function App() {
-  
+
   return (
-   <BrowserRouter>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-    </Routes>
-   </BrowserRouter>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/component/Nav.jsx
@@ -0,0 +1,34 @@
+function Nav() {
+  return (
+    <div>
+      <div>
+        <img src="" alt="" />
+      </div>
+      <div>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+        <a href=""></a>
+      </div>
+      <div>
+        <input type="text" name="" id="" />
+        <div>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+          <a href="">
+            <img src="" alt="" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
